f6c9545323b859c2293ff071db4c2506dd1759ae
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2024-03-23 08:17:19 -0300

    feat: other's info in infoText props

--- src/components/Main/Info/InfoText.tsx
+++ src/components/Main/Info/InfoText.tsx
@@ -3,13 +3,19 @@ import { ReactNode } from "react";
 interface InfoTextProps {
   children: ReactNode;
   size?: number;
+  displayType?: string
+  textAlignType?: string
 }
 
-export default function InfoText({ children, size }: InfoTextProps) {
+export default function InfoText({ children, size, displayType, textAlignType }: InfoTextProps) {
   return (
     <span
-      className="font-roboto text-center inline-block"
-      style={{ fontSize: size != null ? size : 14 }}
+      className="font-roboto text-center"
+      style={{
+        fontSize: size != null ? size : 14,
+        display: displayType != null ? displayType : "inline-block",
+        textAlign: textAlignType != null ? textAlignType : "center"
+      }}
     >
       {children}
     </span>
--- src/components/Main/MainComponent.tsx
+++ src/components/Main/MainComponent.tsx
@@ -1,4 +1,4 @@
-import { BarChart4, Clock, ListTodo, Mail, Phone, User } from "lucide-react";
+import { BarChart4, Gauge, ListTodo, Mail, Phone, ReceiptText, User } from "lucide-react";
 import InfoCard from "./Info/InfoCard";
 import Container from "./Container";
 import InfoText from "./Info/InfoText";
@@ -30,6 +30,11 @@ interface CostumerDataProps {
   email: string,
   name: string,
   cellPhone: string
+}
+
+interface FaqCardsProps {
+  title: string,
+  infoText: ReactElement
 }
 
 export function MainComponent() {
@@ -65,7 +70,7 @@ export function MainComponent() {
 
   const infoCards = [
     {
-      icon: <Clock size={50} color="rgba(18, 117, 246, 0.61)" />,
+      icon: <Gauge size={50} color="rgba(18, 117, 246, 0.61)" />,
       infoText: "Entregamos resultados com rapidez e excelência!",
       size: 16,
     },
@@ -79,8 +84,31 @@ export function MainComponent() {
       infoText: "Impulsionando o crescimento empresarial!",
       size: 16,
     },
+    {
+      icon: <ReceiptText size={50} color="rgba(18, 117, 246, 0.61)" />,
+      infoText: "Documentação completa",
+      size: 16,
+    },
   ];
 
+  const faqCards = [
+    {
+      title: "Quais são as regiões de atendimento?",
+      infoText: <InfoText size={15}>Atualmente trabalhos nas regiões de Coronel Fabriciano, Timoteo e Ipatinga</InfoText>,
+    },
+    {
+      title: "Como a Tech Growth funciona?",
+      infoText: <InfoText size={15} displayType="inherit" textAlignType="start">A Tech Growth, realiza os melhores atendimentos e serviços da região do Vale do Aço. Temos uma vasta gama de serviços e experiencias. Podemos afirmar que realizaremos o melhor serviço para você e a sua empresa</InfoText>,
+    },
+    {
+      title: "Como faço um orçamento?",
+      infoText: <InfoText size={15}>Basta preencher o formulario informando a sua solicitação, ou pelo WhatsApp: (xx) xxxx-xxxx</InfoText>,
+    },
+    {
+      title: "Quais são as formas de pagamento?",
+      infoText: <InfoText size={15}>Você pode pagar em dinheiro, cartão de crédito, a vista ou PIX.</InfoText>,
+    },
+  ]
 
   const [costumerData, setCostumerData] = useState<CostumerDataProps>({
     email: "",
@@ -96,18 +124,26 @@ export function MainComponent() {
 
   return (
     <MainContainer>
-      <section className="!max-w-screen h-fit flex flex-col">
+      <section className="!max-w-full h-fit flex flex-col">
         <Container bg_color="white" customizeHeight="300">
           <InfoTitle>Quem somos?</InfoTitle>
-          <div className="w-[700px] align-baseline">
-            <InfoText size={16}>
-              Somos uma empresa dedicada à resolução de problemas empresariais e residenciais, oferecendo uma ampla gama de serviços. Desde a criação de sites até a infraestrutura de redes e Wi-Fi corporativo, incluindo automação de planilhas. Também realizamos montagem e manutenção de computadores, instalação de softwares e outras configurações essenciais. Nosso compromisso é fornecer soluções abrangentes e eficazes, garantindo eficiência e satisfação aos nossos clientes.
-            </InfoText>
-          </div>
+          <article className="w-[700px] align-baseline">
+            <p>
+              <InfoText size={16}>
+                Somos uma empresa dedicada à resolução de problemas empresariais e residenciais, oferecendo uma ampla gama de serviços. Desde a criação de sites até a infraestrutura de redes e Wi-Fi corporativo, incluindo automação de planilhas. Também realizamos montagem e manutenção de computadores, instalação de softwares e outras configurações essenciais. Nosso compromisso é fornecer soluções abrangentes e eficazes, garantindo eficiência e satisfação aos nossos clientes.
+              </InfoText>
+            </p>
+            <br />
+            <p>
+              <InfoText size={16}>
+                Somos uma equipe de empreendedores liderada por Eduardo, Vitor e Felipe, contando também com um grupo dedicado de funcionários. Juntos, trabalhamos para oferecer soluções excepcionais aos nossos clientes.
+              </InfoText>
+            </p>
+          </article>
         </Container>
         <Container customizeHeight="400">
           <div className="w-full h-full flex flex-col justify-around items-center">
-            <InfoTitle>Adicionar o titulo</InfoTitle>
+            <InfoTitle>Porque escolher a TechGrowth?</InfoTitle>
             <div className="w-full flex gap-3 items-center justify-center">
               {infoCards &&
                 infoCards.map((card: InfoCardProps, index: number) => {
@@ -121,7 +157,7 @@ export function MainComponent() {
           </div>
         </Container>
       </section>
-      <section className="!max-w-screen h-fit flex flex-col">
+      <section className="!max-w-full h-fit flex flex-col">
         <Container
           bg_color="white"
           customizeHeight="750"
@@ -140,7 +176,33 @@ export function MainComponent() {
           </div>
         </Container>
         <Container
+          customizeHeight="550"
+        >
+          <InfoTitle> FAQ </InfoTitle>
+          <div className="h-[500px] !max-w-full w-full flex items-center justify-center">
+            <div className="h-[450px] w-10/12 flex p-2 items-center gap-7 flex-col">
+              {
+                faqCards && faqCards.map((card: FaqCardsProps, key: number) => {
+                  return (
+                    <div key={key} className="rounded-lg w-8/12 min-h-[80px] h-fit flex items-center justify-center gap-3 overflow-hidden p-2 bg-white shadow-md font-roboto">
+                      <details className="w-full transition duration-500 ease-in-out" open={key === 0}>
+                        <summary className="border border-white cursor-pointer">
+                          <span className="font-medium text-[20px]"> {card.title}</span>
+                        </summary>
+                        <div className="w-full h-full p-2 flex items-start justify-start">
+                          {card.infoText}
+                        </div>
+                      </details>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          </div>
+        </Container>
+        <Container
           customizeHeight="450"
+          bg_color="white"
         >
           <form method="post" onSubmit={onSubmitForm} className="p-4 bg-white rounded-md shadow-md shadow-white gap-5 w-2/5 h-full flex flex-col items-center">
             <div className="w-full flex flex-col items-center justify-center gap-5">
